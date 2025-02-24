cee6d14956ecf743670036103cc3b2566f7df96a
aleefbasha123 <aleefbasha123@gmail.com>
2025-02-24 21:57:05 +0530

    some-message

---
+++ backend.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabeling nodejs version 20"
+
+dnf install nodejs -y
+VALIDATE $? "Insatalling nodejs"
+
+useradd expense
+VALIDATE $? "Adding expense user"
+
+#if [ $user -ne 0 ]
+#then
+#    useradd expense
+#else
+#     echo "user alrady exsiting"
+#fi
+
